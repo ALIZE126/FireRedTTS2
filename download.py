8f2eccec9eeaa0787db0f3b5ccc9b08d40839429
lvguangchuan001 <lvguangchuan001@ke.com>
2025-09-20 10:25:49 +0800

    [初始化]服务

---
+++ download.py
@@ -0,0 +1,15 @@
+# 安装
+# pip install -U huggingface_hub
+
+from huggingface_hub import snapshot_download
+
+# 如需代理：
+import os
+# os.environ["HTTP_PROXY"] = "http://127.0.0.1:7890"
+# os.environ["HTTPS_PROXY"] = "http://127.0.0.1:7890"
+
+# 如需镜像（任选其一可用镜像域名）
+os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+
+# 拉取仓库（包含 LFS 大文件，会自动断点续传）
+snapshot_download(repo_id="FireRedTeam/FireRedTTS2", local_dir="FireRedTTS2", local_dir_use_symlinks=False)
